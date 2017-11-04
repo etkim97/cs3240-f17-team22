262270fb13d8813f8a4127ba3154aab77161ae37
jennfang <jjf4bz@virginia.edu>
2017-11-04 00:45:41 -0400

    Add User, Report, and Message models as well as add views for them

---
+++ lokahi/catalog/models.py
@@ -0,0 +1,80 @@
+from django.db import models
+from django.contrib.auth.models import User
+from datetime import date
+
+
+# Create your models here.
+
+
+class User(models.Model):
+	username = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+	first_name = models.CharField(max_length=200)
+	last_name = models.CharField(max_length=200)
+
+	def __str__(self):
+		return "%s - %s - %s - %s" % (self.username, 
+			self.password, self.first_name, self.last_name)	
+
+
+from django.urls import reverse_lazy
+# Used to generate URLS by reversing the URL patterns
+
+class Report(models.Model):
+	"""
+	Model representing a report (but not a specific copy of a book).
+	"""
+	report_name = models.CharField(max_length=200)
+	info = models.TextField(help_text = "Enter information about the business plan and/or project")
+
+	# Need to actually upload the file here
+	filename = models.CharField(max_length=200)
+
+	# Need to set private/public through user settings here
+	privacy_setting = models.CharField(max_length=10)
+
+	# Need to allow investor users to upload files
+
+	def __str__(self):
+		"""
+		String for representing the Model object.
+		"""
+		return self.report_name
+
+	def get_absolute_url(self):
+		"""
+		Returns the url to access a particular report instance.
+		"""
+		return reverse_lazy('report-detail', args=[str(self.id)])
+
+
+# A user can leave a private message for a specific user. A user will have the ability to see a list of
+# messages left for him or her, and be able to delete a message. Private messages will be encrypted, and
+# the recipient can choose to decrypt it and then read 
+
+class Message(models.Model):
+	"""
+	Model representing a message between users
+	"""
+
+	recipient = models.ForeignKey(User, on_delete = models.CASCADE)
+	message_body = models.TextField(help_text = "Enter your private message.")
+
+	def __str__(self):
+		"""
+		String for representing the Model object.
+		"""
+		return "Message for " + self.recipient
+
+	def get_absolute_url(self):
+		"""
+		Returns the url to access a particular report instance.
+		"""
+		return reverse_lazy('message-detail', args=[str(self.id)])
+
+
+
+
+
+
+
---
+++ lokahi/catalog/views.py
@@ -0,0 +1,116 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import User, Report, Message
+
+def index(request):
+    """
+    View function for home page of site.
+    """
+    # Generate counts of some of the main objects
+    # num_reports=Report.objects.all().count()
+    # num_messages=Message.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+    
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={'num_visits':num_visits}, 
+    )
+
+
+
+from django.views import generic
+
+class ReportListView(generic.ListView):
+    model = Report
+    paginate_by = 10
+
+class ReportDetailView(generic.DetailView):
+    model = Report
+
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+class ReportsByUserListView(LoginRequiredMixin,generic.ListView):
+    """
+    Generic class-based view accessible reports to current user. 
+    """
+    model = Report
+    template_name ='catalog/list_reports.html'
+    paginate_by = 10
+    
+    # Need to get user reports by privacy accessibility
+
+    # def get_queryset(self):
+    #     return Report.objects.filter(user=self.request.user).filter(status__exact='o').order_by('due_back')
+
+
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+class MessageCreate(CreateView):
+    model = Message
+    fields = '__all__'
+    success_url = '/'
+
+
+class MessageUpdate(UpdateView):
+    model = Message
+    fields = ['recipient', 'message_body']
+    success_url = '/'
+
+class MessageDelete(DeleteView):
+    model = Message
+    success_url = reverse_lazy('messages')
+
+
+
+from django.contrib.auth.decorators import permission_required
+
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+import datetime
+
+from .forms import RenewBookForm
+
+
+# Use this template to update reports
+
+# @permission_required('catalog.can_mark_returned')
+# def renew_book_librarian(request, pk):
+#     """
+#     View function for renewing a specific BookInstance by librarian
+#     """
+#     book_inst=get_object_or_404(BookInstance, pk = pk)
+
+#     # If this is a POST request then process the Form data
+#     if request.method == 'POST':
+
+#         # Create a form instance and populate it with data from the request (binding):
+#         form = RenewBookForm(request.POST)
+
+#         # Check if the form is valid:
+#         if form.is_valid():
+#             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+#             book_inst.due_back = form.cleaned_data['renewal_date']
+#             book_inst.save()
+
+#             # redirect to a new URL:
+#             return HttpResponseRedirect('/')
+
+#     # If this is a GET (or any other method) create the default form.
+#     else:
+#         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
+#         form = RenewBookForm(initial={'renewal_date': proposed_renewal_date,})
+
+#     return render(request, 'catalog/book_renew_librarian.html', {'form': form, 'bookinst':book_inst})
+
+
+
