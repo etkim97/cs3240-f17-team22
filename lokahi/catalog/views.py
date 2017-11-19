3bdc62b52369ed945148d4860e579a60c5132aa8
gina <gmp5vb@virginia.edu>
2017-11-19 18:57:45 -0500

    fixing broken log in

--- lokahi/catalog/views.py
+++ lokahi/catalog/views.py
@@ -46,43 +46,25 @@ def createAccount(request):
 		)
 
 def signup(request):
-	if request.method == 'POST':
-		form = signUp(request.POST)
-		if form.is_valid():
-			user = User.objects.create_user(form.cleaned_data['username'])
-			user.first_name = form.cleaned_data['first_name']
-			user.last_name = form.cleaned_data['last_name']
-			user.company = form.cleaned_data['company']
-			user.password=form.cleaned_data['password']
-			user.email = form.cleaned_data['email']
-			user.user_type = form.cleaned_data['user_type']
-			user.save()
-			# print(form.cleaned_data['username'])
-			return redirect('/')
-	else:
-		form = signUp()
-	return render(request, 'signup.html', {'form': form})
-
-# =======
-#     if request.method == 'POST':
-#         form = signUp(request.POST)
-#         if form.is_valid():
-#            user = User.objects.create_user(form.cleaned_data['username'])
-#            # USERNAME_FIELD = form.cleaned_data['username']
-#            user.first_name = form.cleaned_data['first_name']
-#            user.last_name = form.cleaned_data['last_name']
-#            user.company = form.cleaned_data['company']
-#            passw = make_password(form.cleaned_data['password'])
-#            user.password = passw
-#            user.email = form.cleaned_data['email']
-#            user.user_type = form.cleaned_data['user_type']
-#            user.save()
-#            login(request, user)
-#            # print(form.cleaned_data['username'])
-#            return redirect('/')
-#     else:
-#         form = signUp()
-#     return render(request, 'signup.html', {'form': form})
+    if request.method == 'POST':
+        form = signUp(request.POST)
+        if form.is_valid():
+           user = User.objects.create_user(form.cleaned_data['username'])
+           # USERNAME_FIELD = form.cleaned_data['username']
+           user.first_name = form.cleaned_data['first_name']
+           user.last_name = form.cleaned_data['last_name']
+           user.company = form.cleaned_data['company']
+           passw = make_password(form.cleaned_data['password'])
+           user.password = passw
+           user.email = form.cleaned_data['email']
+           user.user_type = form.cleaned_data['user_type']
+           user.save()
+           login(request, user)
+           # print(form.cleaned_data['username'])
+           return redirect('/')
+    else:
+        form = signUp()
+    return render(request, 'signup.html', {'form': form})
 
 def log_in(request, template_name="registration/login.html"):
     if request.method == "POST":
